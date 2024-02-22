399afe20f5e14d324f0267ae141c616246faf2bd
Kulakov-Nikita <kulakofnikita@gmail.com>
2024-02-22 10:59:41 +0300

    [fix] Генератор уровня

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    [SerializeField] private int sizeX = 10, sizeY = 10;
+    [SerializeField] private int sizeX = 16, sizeY = 8;
     [SerializeField] private GameObject defaultCell, moverRight, moverLeft;
     [SerializeField] private GameObject player;
-    [SerializeField] private int playerPosX = 10, playerPosY = 10;
+    [SerializeField] private int playerPosX = 3, playerPosY = 3;
     void Start()
     {
-        for (int y = -sizeY / 2; y <= sizeY / 2; y++)
+        for (int y = 0; y <= sizeY; y++)
         {
-            for (int x = -sizeX / 2; x <= sizeX / 2; x++)
+            for (int x = 0; x <= sizeX; x++)
             {
                 switch(Random.Range(0, 3))
                 {
-                    case 0: Instantiate(defaultCell, new Vector3(x, y, 0), new Quaternion(0, 0, 0, 0)); break;
-                    case 1: Instantiate(moverRight, new Vector3(x, y, 0), new Quaternion(0, 0, 0, 0)); break;
-                    case 2: Instantiate(moverLeft, new Vector3(x, y, 0), new Quaternion(0, 0, 0, 0)); break;
+                    case 0: Instantiate(defaultCell, new Vector3(x - sizeX / 2, y - sizeY / 2, 0), new Quaternion(0, 0, 0, 0)); break;
+                    case 1: Instantiate(moverRight, new Vector3(x - sizeX / 2, y - sizeY / 2, 0), new Quaternion(0, 0, 0, 0)); break;
+                    case 2: Instantiate(moverLeft, new Vector3(x - sizeX / 2, y - sizeY / 2, 0), new Quaternion(0, 0, 0, 0)); break;
                 }
             }
         }
